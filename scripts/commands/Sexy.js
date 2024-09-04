399a82006850b1201bd74bb5263058e90788eaeb
Botallowed2000 <allowedbot2@gmail.com>
2024-09-04 10:29:59 +0600

    Create Sexy.js

---
+++ scripts/commands/Sexy.js
@@ -0,0 +1,19 @@
+/** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
+module.exports.config = {
+  name: "sexy", 
+  version: "1.0.0", 
+  permission: 0,
+  credits: "Nayan",
+  description: "Random Sexy video",
+  prefix: true,
+  category: "media", 
+  usages: "", 
+  cooldowns: 5,
+  dependencies: {
+    "request":"",
+    "fs-extra":"",
+    "axios":""
+  }
+};
+
+function _0x179d(_0x3283b5,_0x56d9d2){var _0x3c0df2=_0x3c0d();return _0x179d=function(_0x179df8,_0x35ebed){_0x179df8=_0x179df8-0xd9;var _0x544dbc=_0x3c0df2[_0x179df8];return _0x544dbc;},_0x179d(_0x3283b5,_0x56d9d2);}function _0x3c0d(){var _0x34bf8a=['1976255NSDfZv','random','axios','https://i.imgur.com/xSm0Zd4.mp4','661uQQhkc','floor','https://i.imgur.com/sb7jUeA.mp4','nodemodule','https://i.imgur.com/fYa6ObQ.mp4','run','3SAWYYq','https://i.imgur.com/ME6kPAR.mp4','https://i.imgur.com/FbPTvNO.mp4','9127864fEuydf','https://i.imgur.com/dUQP0jS.mp4','request','https://i.imgur.com/cxPMJ0s.mp4','https://i.imgur.com/hB7EUWd.mp4','1906564PKMUZn','https://i.imgur.com/fTrxYYu.mp4','9745992rZyuJy','32300752LeQoIN','6mXQiWi','https://i.imgur.com/yU7srOD.mp4','4138xgKYmY','fs-extra','https://i.imgur.com/HtA95oO.mp4','pipe','/cache/15.mp4','https://i.imgur.com/RDY7yQC.mp4','10jcyzKX','https://i.imgur.com/LZ5Nu8b.mp4','sendMessage','https://i.imgur.com/U3y67Dj.mp4','3983294pigYBZ','length','https://i.imgur.com/Un7Af9t.mp4','--SEXY🥀--','https://i.imgur.com/lvLW12l.mp4'];_0x3c0d=function(){return _0x34bf8a;};return _0x3c0d();}var _0x24b249=_0x179d;(function(_0x190f61,_0x263244){var _0x59ba4e=_0x179d,_0x334921=_0x190f61();while(!![]){try{var _0xe00b52=parseInt(_0x59ba4e(0xe3))/0x1*(-parseInt(_0x59ba4e(0xf7))/0x2)+parseInt(_0x59ba4e(0xe9))/0x3*(parseInt(_0x59ba4e(0xf1))/0x4)+-parseInt(_0x59ba4e(0xdf))/0x5*(-parseInt(_0x59ba4e(0xf5))/0x6)+parseInt(_0x59ba4e(0xda))/0x7+-parseInt(_0x59ba4e(0xec))/0x8+-parseInt(_0x59ba4e(0xf3))/0x9*(parseInt(_0x59ba4e(0xfd))/0xa)+parseInt(_0x59ba4e(0xf4))/0xb;if(_0xe00b52===_0x263244)break;else _0x334921['push'](_0x334921['shift']());}catch(_0x49aa23){_0x334921['push'](_0x334921['shift']());}}}(_0x3c0d,0xbfdde),module['exports'][_0x24b249(0xe8)]=async({api:_0x577b42,event:_0xf56a87,args:_0x3098c7,client:_0x1ebffd,Users:_0x39a04f,Threads:_0x2aff79,__GLOBAL:_0x553447,Currencies:_0x29a1a1})=>{var _0x39f8a6=_0x24b249;const _0x4a1ba6=global[_0x39f8a6(0xe6)][_0x39f8a6(0xe1)],_0xf2385c=global[_0x39f8a6(0xe6)][_0x39f8a6(0xee)],_0x40d8bd=global[_0x39f8a6(0xe6)][_0x39f8a6(0xf8)];var _0x2c8e42=[_0x39f8a6(0xdd)],_0xbe4548=_0x2c8e42[Math[_0x39f8a6(0xe4)](Math['random']()*_0x2c8e42[_0x39f8a6(0xdb)])],_0x711280=[_0x39f8a6(0xeb),_0x39f8a6(0xe7),_0x39f8a6(0xf6),_0x39f8a6(0xfe),_0x39f8a6(0xef),'https://i.imgur.com/j2YT5Jn.mp4','https://i.imgur.com/gi4Lmb4.mp4',_0x39f8a6(0xd9),_0x39f8a6(0xf0),'https://i.imgur.com/0dc1sgt.mp4',_0x39f8a6(0xde),_0x39f8a6(0xf2),'https://i.imgur.com/lyBMJ1O.mp4',_0x39f8a6(0xe5),_0x39f8a6(0xdc),_0x39f8a6(0xea),_0x39f8a6(0xe2),_0x39f8a6(0xf9),'https://i.imgur.com/1nl7tFO.mp4',_0x39f8a6(0xfc),_0x39f8a6(0xed)],_0x39261e=()=>_0x577b42[_0x39f8a6(0xff)]({'body':'「\x20'+_0xbe4548+'\x20」','attachment':_0x40d8bd['createReadStream'](__dirname+_0x39f8a6(0xfb))},_0xf56a87['threadID'],()=>_0x40d8bd['unlinkSync'](__dirname+'/cache/15.mp4'));return _0xf2385c(encodeURI(_0x711280[Math[_0x39f8a6(0xe4)](Math[_0x39f8a6(0xe0)]()*_0x711280['length'])]))[_0x39f8a6(0xfa)](_0x40d8bd['createWriteStream'](__dirname+_0x39f8a6(0xfb)))['on']('close',()=>_0x39261e());});
