55667734af45cf791f8d6988c7dfabec6c353847
Botallowed2000 <allowedbot2@gmail.com>
2024-09-11 18:54:24 +0600

    Update funnyvideo.js

--- scripts/commands/funnyvideo.js
+++ scripts/commands/funnyvideo.js
@@ -20,26 +20,26 @@ module.exports.run = async ({ api, event, args, client, Users, Threads, __GLOBAL
   const request = global.nodemodule["request"];
   const fs = global.nodemodule["fs-extra"];
   
-  var hi = ["--FUNNY-VIDEO_FARHAN-ISLAM--"];
+  var hi = ["--FUNNY-VIDEO_SAGOR-ISLAM--"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
-    "https://drive.google.com/uc?id=1Zg6YCrfLNFVPuIarV3ZBvyg9NW9vKf-i",
-    "https://drive.google.com/uc?id=1Tu7vjhlkUls3SKSTl-pGK3y69NYgeGMe",
-    "https://drive.google.com/uc?id=1vHhwiHHDRJpflMGCU0Alg7A5ARkugLya", 
-    "https://drive.google.com/uc?id=1KrHanrUqkqr0kFjFh1abl72xlmZ0_18a",
-    "https://drive.google.com/uc?id=1rs6cbx8oOYg2Zgi_0UZHfbDhEz8LjFlU",
-    "https://drive.google.com/uc?id=1thJh4_fG8DYdgKiOhsy8Jkp98O0m-23b",
-    "https://drive.google.com/uc?id=1T5x_hAEu5yozou0HeNrCHC6GS3XbgTSx",
-    "https://drive.google.com/uc?id=1CRvedhuz9z2JWLY6LH2dNgtt7cwuBBsG",
-    "https://drive.google.com/uc?id=1RbPFrHj4y7eno8OsAuYElOfdOsJ75eZp",
-    "https://drive.google.com/uc?id=1mY0B0yGi90h0K1GvxVdZ7eLkj-Q-W2Eq",
-    "https://drive.google.com/uc?id=1xgh5EePrQq62zeDRu2YAkJTrAXSCXpOp",
-    "https://drive.google.com/uc?id=1-aZjX6vnC1HDn25jBoexmyLBlm6bLwli",
-    "https://drive.google.com/uc?id=1znMcAJbcDnS0oDG6LCUH8PN0gZOJxhRC", 
-    "https://drive.google.com/uc?id=1teEOVYZwvGuz75_Is_ZEEvZwroB1IZW8", 
-    "https://drive.google.com/uc?id=10gQjKcAL8MkXOqi8vLYqPYiFg0_Qh-rR", 
-    "https://drive.google.com/uc?id=1b0xOpxhPq0xZO7QDpU4BZ-OnRKYPMdLD", 
-    "https://drive.google.com/uc?id=1-KLse2-7YKacnPGL7zHH5_KOHQUbVUt0", 
+    "https://i.imgur.com/0Nqkbez.mp4",
+    "https://i.imgur.com/8NOCa6Q.mp4",
+    "https://i.imgur.com/2f1Ldc4.mp4", 
+    "https://i.imgur.com/NIwlzYS.mp4",
+    "https://i.imgur.com/2Ex7jc2.mp4",
+    "https://i.imgur.com/Bc6fF5c.mp4",
+    "https://i.imgur.com/N7N36mP.mp4",
+    "https://i.imgur.com/GICmTcH.mp4",
+    "https://i.imgur.com/RQPW4J4.mp4",
+    "",
+    "",
+    "",
+    "", 
+    "", 
+    "", 
+    "", 
+    "", 
   ];
   
   var randomLink = link[Math.floor(Math.random() * link.length)];
